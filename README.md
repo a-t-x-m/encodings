c0cd59371823d0d1b1c0ae3e6c722440b2947ba7
idleberg <git@idleberg.com>
2020-02-21 10:05:56 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,33 @@
+# @atxmtx/encodings
+
+[![npm](https://flat.badgen.net/npm/license/@atxmtx/encodings)](https://www.npmjs.org/package/@atxmtx/encodings)
+[![npm](https://flat.badgen.net/npm/v/@atxmtx/encodings)](https://www.npmjs.org/package/@atxmtx/encodings)
+[![CircleCI](https://flat.badgen.net/circleci/github/atxmtx/encodings)](https://circleci.com/gh/atxmtx/encodings)
+[![David](https://flat.badgen.net/david/dep/atxmtx/encodings)](https://david-dm.org/atxmtx/encodings)
+
+List of supported Atom text encodings
+
+## Installation
+
+`npm install @atxmtx/encodings -S`
+
+## Usage
+
+**Examples**:
+
+```js
+// JavaScript
+import { encodings } from '@atxmtx/encodings';
+
+console.log('List of supported encodings', encodings);
+```
+
+```coffee
+# CoffeeScript
+const { encodings } = require "@atxmtx/encodings"
+
+console.log "List of supported encodings", encodings
+
+## License
+
+This work is dual-licensed under [The MIT License](https://opensource.org/licenses/MIT) and the [GNU General Public License, version 2.0](https://opensource.org/licenses/GPL-2.0)
---
+++ lib/index.d.ts
@@ -0,0 +1,204 @@
+declare const encodings: {
+    utf8: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    utf16le: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    utf16be: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1252: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88591: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88593: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso885915: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    macroman: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    cp437: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    cp850: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1256: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88596: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1257: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88594: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1250: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88592: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1251: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    cp866: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88595: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    koi8r: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    koi8u: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso885913: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1253: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88597: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1255: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88598: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso885916: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1254: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso88599: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    windows1258: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    gbk: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    gb18030: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    cp950: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    big5hkscs: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    shiftjis: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    cp932: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    eucjp: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    euckr: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso885914: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+    iso885910: {
+        list: string;
+        status: string;
+        posixOnly: boolean;
+    };
+};
+declare function getActiveTextEditorEncoding(): any;
+export { encodings, getActiveTextEditorEncoding };
